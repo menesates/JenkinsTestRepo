9be8c22039185f4b7ceab6e3e26cd99328a9e87b
heulahe <atesmuhamemdenes@gmail.com>
2019-02-01 20:48:01 +0300

    pmd test class, value and method

--- src/main/java/com/menesates/jenkinstest/JenkinsTestClass.java
+++ src/main/java/com/menesates/jenkinstest/JenkinsTestClass.java
@@ -4,7 +4,6 @@ public class JenkinsTestClass {
 
     private static int a = 6;
     private static int b = 4;
-    private static int c = 10;
 
 
     public static void main(String[] args){
---
+++ src/main/java/com/menesates/jenkinstest/PmdTestClass.java
@@ -0,0 +1,11 @@
+package com.menesates.jenkinstest;
+
+public class PmdTestClass {
+
+    private int a = 0;
+
+    public void test(){
+        System.out.println("test");
+    }
+
+}
