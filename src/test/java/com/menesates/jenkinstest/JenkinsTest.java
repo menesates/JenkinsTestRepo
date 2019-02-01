9a643bad34eb14d72d5db1c3f3894414e42b991a
heulahe <atesmuhamemdenes@gmail.com>
2019-02-01 16:50:05 +0300

    first commit

---
+++ src/test/java/com/menesates/jenkinstest/JenkinsTest.java
@@ -0,0 +1,17 @@
+package com.menesates.jenkinstest;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class JenkinsTest {
+
+    @Test
+    public void testSucces(){
+        Assert.assertEquals(2,2);
+    }
+
+    /*@Test
+    public void testFail(){
+        Assert.assertEquals(2,3);
+    }*/
+}
