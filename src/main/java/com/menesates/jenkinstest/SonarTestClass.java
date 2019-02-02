2554cd55d9dabc1c22da01bea3385f54a2e85d28
heulahe <atesmuhamemdenes@gmail.com>
2019-02-02 15:28:11 +0300

    sonar test

---
+++ src/main/java/com/menesates/jenkinstest/SonarTestClass.java
@@ -0,0 +1,12 @@
+package com.menesates.jenkinstest;
+
+public class SonarTestClass {
+
+    public int a = 5;
+
+    public void testSonar(){
+        while (true){
+            
+        }
+    }
+}
